ab444d18f34dad0ac0706c483488f5e979d92e45
duster5070 <ahmedscorpion15@gmail.com>
2025-12-25 22:22:53 +0200

    add guard for authenticating and authorize user using tokens and roles

--- src/common/guards/auth/auth.guard.ts
+++ src/common/guards/auth/auth.guard.ts
@@ -1,11 +1,63 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { UserRole } from '@/modules/users/enums/user-role.enum';
+import { JWTPayloadType } from '@/common/auth/types/jwt-payload.type';
+import { UsersService } from '@/modules/users/users.service';
+import { ROLES_KEY } from '@/common/auth/decorators/roles.decorator';
+import { CanActivate, ExecutionContext, ForbiddenException, forwardRef, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+export class AuthRolesGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly reflector: Reflector,
+    @Inject(forwardRef(() => UsersService))
+    private readonly userService: UsersService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException('Access denied, no token provided');
+    }
+
+    try {
+      const payload: JWTPayloadType = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      });
+
+      const user = await this.userService.getCurrentUser(payload.id);
+      if (!user) {
+        throw new UnauthorizedException('Access denied, user not found');
+      }
+      request['user'] = payload;
+
+      const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+        ROLES_KEY,
+        [context.getHandler(), context.getClass()],
+      );
+      if (requiredRoles && requiredRoles.length > 0) {
+        if (!requiredRoles.includes(user.role)) {
+          throw new ForbiddenException('Access denied, insufficient permissions');
+        }
+      }
+
+      return true;
+    } catch (error) {
+      if (error instanceof UnauthorizedException || error instanceof ForbiddenException) {
+        throw error;
+      }
+      throw new UnauthorizedException('Access denied, invalid token');
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
